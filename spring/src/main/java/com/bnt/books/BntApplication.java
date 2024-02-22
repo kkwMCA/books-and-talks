c2d82b56c37c36e95b7299eedb1de3c65e48884c
Sumedh ahire <ahiresumedh40@gmail.com>
2024-02-22 22:24:54 +0530

    spring

---
+++ spring/src/main/java/com/bnt/books/BntApplication.java
@@ -0,0 +1,13 @@
+package com.bnt.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BntApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/bnt/books/BntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnt.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
