6fa89f67943190aeb88f4ae1ad8ae63b8ceaac40
Sumedh ahire <ahiresumedh40@gmail.com>
2024-02-23 21:31:36 +0530

    changes in package

--- spring/src/main/java/com/bnt/books/ControllerClass.java
+++ spring/src/main/java/com/bnt/books/ControllerClass.java
@@ -3,8 +3,6 @@ package com.bnt.books;
 import java.util.HashMap;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
