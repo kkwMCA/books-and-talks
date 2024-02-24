ed5802a5e3fbf619ea0d2081ebee58b2b41a5fb5
Sumedh ahire <ahiresumedh40@gmail.com>
2024-02-24 10:00:36 +0530

    bunch of apis to learn

---
+++ spring/src/main/java/com/bnt/books/TestingControllers/ControllerClass.java
@@ -0,0 +1,47 @@
+package com.bnt.books.TestingControllers;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+class ControllerClass{
+    //to get data 
+    @GetMapping("/getData")
+    public Object getSomething(){
+        HashMap hashMap=new HashMap<>();
+        hashMap.put("name", "pratik");
+        return hashMap;
+    } 
+
+    //send data
+    @PostMapping("/send")
+    public void postMethodName(@RequestBody HashMap<String,Object> hashMap) {
+        //TODO: process POST request
+        System.out.println(hashMap);
+        //return entity;
+    }
+    
+    //send and get data
+    @PostMapping("/sendGet")
+    public HashMap sendGet(@RequestBody HashMap<String,Object> hashMap) {
+        //TODO: process POST request
+        System.out.println(hashMap);
+        //return entity;
+        return hashMap;
+    }
+
+    //send data as parameter
+    @GetMapping("/getPara")
+    public String getPara(@RequestParam(value = "id") String id) {
+        return id;
+    }
+    
+
+}
