b2dc480f3cea098468b77ec8ef2a72df59d70871
Sumedh ahire <ahiresumedh40@gmail.com>
2024-03-10 12:07:46 +0530

    colan again

--- spring/src/main/java/com/bnt/books/Security/WebConfig.java
+++ spring/src/main/java/com/bnt/books/Security/WebConfig.java
@@ -10,7 +10,7 @@ class WebConfig implements WebMvcConfigurer{
    @Override
    public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**")
-         .allowedOrigins("https://:localhost:3000")  // Replace with the actual URL of your React app
+         .allowedOrigins("http://localhost:3000")  // Replace with the actual URL of your React app
          .allowedMethods("GET", "POST", "PUT", "DELETE")
          .allowedHeaders("*");
    }
