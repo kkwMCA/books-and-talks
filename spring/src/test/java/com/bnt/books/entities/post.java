acf4ea981807d95360773ba9050e237e74c48a78
Madhura Ashture <madhuraashturkar2468@gmail.com>
2024-04-15 22:24:46 +0530

    Added Lombok dependency and @NoArgsConstructor , @Getter , @Setter annotations of Lombok

--- spring/src/test/java/com/bnt/books/entities/post.java
+++ spring/src/test/java/com/bnt/books/entities/post.java
@@ -8,8 +8,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "post")
 public class post {
     @Id
@@ -24,3 +30,26 @@ public class post {
     private Timestamp datetime;
 
 }
+
+/*
+ * @Entity annotation: is used to mark this class as a JPA entity.
+ * It indicates that instances of this class will be mapped to database records.
+ * 
+ * @Table(name = "table_name")
+ * make sure this matches the actual table name in DB
+ * 
+ * @Id : indicites the primary key.
+ * Every JPA enttity should have the primary key.
+ * 
+ * @GeneratedValue :
+ * This annotation specifies how to the primary key values are generated on
+ * auto-incrementation
+ * 
+ * @Column : used to map a class field to a database column.
+ * 
+ * @NoArgsConstructor : No Argument Constructor (Lombok)
+ * 
+ * @Getter (Lombok)
+ * 
+ * @Setter (Lombok)
+ */
