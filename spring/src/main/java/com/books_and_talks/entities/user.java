68af4d10cf1f6eba43465aec7fb417dc1d4b7aff
Madhura Ashture <madhuraashturkar2468@gmail.com>
2024-04-24 12:53:45 +0530

    deleted and added

---
+++ spring/src/main/java/com/books_and_talks/entities/user.java
@@ -0,0 +1,15 @@
+package com.books_and_talks.entities;
+
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "user")
+public class user {
+
+}
