f4c0ebd81ed6798ae636552ef3bd622d452710a9
pratikk2001 <pratikkankarej18@gmail.com>
2024-04-26 15:46:35 +0530

    testing

--- material/social/src/components/feed/Feed.jsx
+++ material/social/src/components/feed/Feed.jsx
@@ -1,7 +1,23 @@
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-import { Posts } from "../../dummyData";
+// import { Posts } from "../../dummyData";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const url = "http://localhost:8081";
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d))
+  }
+
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
 
 export default function Feed() {
   return (
