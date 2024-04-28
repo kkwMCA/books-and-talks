76cd85bc898b3dbce784cbda54526d78c2cb5c2c
Sumedh ahire <94281812+sumedhahire@users.noreply.github.com>
2024-04-28 10:29:57 +0530

    kese to bhi solve hoya

--- material/social/src/components/feed/Feed.jsx
+++ material/social/src/components/feed/Feed.jsx
@@ -8,22 +8,65 @@ import axios from "axios";
 const baseURL = "http://localhost:8081/getAllPost";
 export default function Feed() {
 
-  const [post, setPost] = React.useState(null);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get(baseURL);
+      setData(response.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
+  useEffect(() => {
+    fetchData();
   }, []);
 
-  if (!post) return null;
+  if (isLoading) return <div>Loading...</div>;
 
+  if (error) return <div>Error: {error.message}</div>;
+
+  if (!data) return null;
+  function traverse_it(obj){
+    for(var prop in obj){
+        if(typeof obj[prop]=='object'){
+            // object
+            traverse_it(obj[prop[i]]);
+        }else{
+            // something else
+            alert('The value of '+prop+' is '+obj[prop]+'.');
+        }
+    }
+}
+  console.log(traverse_it(data))
   return (
+    
     <div>
-      <h1>{post.username}</h1>
-      
+      <h1>Name: {data.postname}</h1>
     </div>
   );
+  
+};
+// axios({
+//   method: "post",
+//   url: "http://localhost:8093/send",
+//   data: newContact,
+//   headers: { "Content-Type": "application/json" },
+
+// })
+//   .then((response) => {
+//     // setResponse(response.data)
+//     console.log(response.data, 'from post method')
+//   })
+//   .catch(function (error) {
+//     console.log(error, 'this is Error');
+//     // console.log(JSON.stringify(newContact));
+//   });
   // return (
   //   <div className="feed">
   //     <div className="feedWrapper">
@@ -32,4 +75,6 @@ export default function Feed() {
   //     </div>
   //   </div>
   // );
+{
+{},{}
 }
